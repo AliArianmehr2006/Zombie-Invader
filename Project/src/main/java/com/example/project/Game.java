9356928588912e4324301da2bd6ac05511e64212
AliArianmehr2006 <ali.arianmehr.2006@gmail.com>
2025-06-07 20:53:06 +0330

    اضافه کردن اهنک

--- Project/src/main/java/com/example/project/Game.java
+++ Project/src/main/java/com/example/project/Game.java
@@ -11,11 +11,13 @@ public class Game extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/project/playerSelection.fxml"));
         Parent root = loader.load();
-
         Scene scene = new Scene(root);
         stage.setTitle("Zombie-Invader");
         stage.setScene(scene);
         stage.show();
+
+        SoundThread t = new SoundThread();
+        t.start();
     }
 
     public static void main(String[] args) {
